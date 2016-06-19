359f34a0012bd58c86a5c171695abf8874a7e7b2
chaoskagami <chaos.kagami@gmail.com>
2016-06-18 21:05:23 -0400

    Strip i2c handling out completely

--- main.c
+++ main.c
@@ -35,9 +35,7 @@
 #include <unistd.h>
 
 #include "bdf.h"
-#include "li2c.h"
 #include "rterm.h"
-#include "ossd_i2c.h"
 
 #define DISPLAY_FONT 0x80000000
 
@@ -65,11 +63,6 @@ static void usage(const char *name)
 	printf("  all:        print all glyphs, not just 32-126\n");
 	printf("  native:     do not adjust font height 8 pixels\n");
 	printf("  ascender H: add extra ascender of H pixels per glyph\n");
-	printf("  rotate:     rotate glyphs' bitmaps CCW\n");
-	printf("  display     show converted font on SSD1306 compatible display\n");
-	printf("  i2c_bus B:  I2C bus for SSD1306 compatible display (default 1)\n");
-	printf("  i2c_addr A: I2C address for SSD1306 compatible display (default 0x3C)\n");
-	printf("  updown:     display orientation is upside down\n");
 }
 
 int main(int argc, char **argv)
@@ -77,11 +70,6 @@ int main(int argc, char **argv)
 	char *file;
 	bdfe_t *font;
   	int flags = 0;
-	uint8_t driver = OSSD_SSD1306;
-	uint8_t i2c_bus = 1;
-	uint8_t i2c_address = 0x3C;
-	uint8_t orientation = 0;
-	uint32_t gidx = 0;
 	unsigned ascender = 0;
 	unsigned gmin = 32, gmax = 126;
 
@@ -137,31 +125,8 @@ int main(int argc, char **argv)
 		if (arg_is(argv[i], "-r", "rotate"))
 			flags |= BDF_ROTATE;
 
-		if (arg_is(argv[i], "-3", "ssd1306"))
-			driver = OSSD_SSD1306;
-
-		if (arg_is(argv[i], "-1", "sh1106"))
-			driver = OSSD_SH1106;
-
-		if (arg_is(argv[i], "-B", "i2c_bus")) {
-			i++;
-			uint32_t i2bus = strtoul(argv[i], NULL, 16);
-			if (i2bus && (i2bus < LI2C_MAX_BUS))
-				i2c_bus = i2bus;
-		}
-
-		if (arg_is(argv[i], "-A", "i2c_addr")) {
-			i++;
-			uint32_t i2ca = strtoul(argv[i], NULL, 16);
-			if (i2ca && (i2ca < 0x78))
-				i2c_address = i2ca;
-		}
-
 		if (arg_is(argv[i], "-d", "display"))
 			flags |= DISPLAY_FONT;
-
-		if (arg_is(argv[i], "-u", "updown"))
-			orientation = OSSD_UPDOWN;
 	}
 
 	file = argv[argc - 1];
@@ -172,71 +137,6 @@ int main(int argc, char **argv)
 		return -1;
 	}
 
-	if (!(flags & DISPLAY_FONT)) {
-		free(font);
-		return 0;
-	}
-
-	if (i2c_bus > PI2C_MAX_BUS)
-		li2c_init(LI2C_EDISON);
-	else
-		li2c_init(LI2C_RPI);
-
-	if (li2c_open(i2c_bus) < 0) {
-		fprintf(stderr, "Unable to open i2c bus %d\n", i2c_bus);
-		free(font);
-		return -1;
-	}
-	if (li2c_select(i2c_bus, i2c_address) < 0) {
-		fprintf(stderr, "Unable to open select device at %02X\n", i2c_address);
-		free(font);
-		return -1;
-	}
-
-	ossd_font_t of;
-	of.gw   = font->gw;
-	of.gh   = font->bpg;
-	of.go   = (uint8_t)gmin;
-	of.gn   = (uint8_t)font->chars;
-	of.font = font->font;
-
-	ossd_init(driver, i2c_bus, orientation);
-	ossd_set_user_font(&of, NULL);
-	ossd_select_font(OSSD_FONT_USER);
-
-	int gh = (of.gh + 7)/8; // glyph height in lines
-
-	char buf[16];
-	sprintf(buf, "%dx%d", of.gw, of.gh);
-	file = basename(file);
-	ossd_putlx(0, -1, file, OSSD_TEXT_REVERSE);
-	ossd_putlx(8 - gh, -1, buf, OSSD_TEXT_UNDERLINE | OSSD_TEXT_OVERLINE);
-	buf[1] = '\0';
-
-	stdin_mode(TERM_MODE_RAW);
-	fprintf(stderr, "Press any key to continue, 'q' to exit\n");
-	if (stdin_getch(-1) == 'q')	goto exit;
-
-	ossd_fill_screen(0);
-
-	do {
-		if (gidx > 0) {
-			fprintf(stderr, "Press any key to continue, 'q' to exit\n");
-			if (stdin_getch(-1) == 'q')	break;
-			ossd_fill_screen(0);
-		}
-		for(int line = 0, l = 0; line < 8; line += gh, l++) {
-			for(int i = 0; i < (128/of.gw) && gidx < font->chars; i++, gidx++) {
-				buf[0] = gidx + of.go;
-				ossd_putlx(line, i*of.gw, (const char *)buf, 0);
-			}
-		}
-	} while(gidx < font->chars);
-
-exit:
-	stdin_mode(TERM_MODE_CAN);
-	li2c_close(i2c_bus);
 	free(font);
-
 	return 0;
 }
